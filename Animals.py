ece378a8c741d7e5fa38ae1c4532d0da404ae07c
NikKar <nikita.karuzin@gmail.com>
2023-10-02 14:30:12 -0600

    Implemented paragraphs to Animals.py file

--- Animals.py
+++ Animals.py
@@ -9,7 +9,17 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     filename = filename.capitalize()
     pdf.add_page()
+
+    # Add title
     pdf.set_font("Times", "B", 24)
-    pdf.set_text_color(100, 100, 100)
-    pdf.cell(0, 12, filename)
+    pdf.set_text_color(102, 178, 255)
+    pdf.cell(0, 12, filename, ln=1)
+
+    # Add paragraph
+    with open(filepath, "r") as file:
+        text = file.read()
+    pdf.set_font("Times", "", 10)
+    pdf.set_text_color(153, 204, 255)
+    pdf.multi_cell(0, 6, text)
+
 pdf.output("Animals.pdf")
